d302952813f1326b5f12d2163e9d0595e33e870a
Laurent Van Acker <laurentvanacker@gmail.com>
2020-08-21 10:25:43 +0200

    added chip drop system

--- src/game/chips/Chip.gd
+++ src/game/chips/Chip.gd
@@ -11,8 +11,27 @@ func _ready():
 	set_color(color)
 
 
+func set_type(type: String):
+	match type:
+		"HP":
+			set_color("blue")
+		"SPEED":
+			set_color("green")
+		"HEAL":
+			set_color("cyan")
+		"ATTACK":
+			set_color("red")
+		"DEFENSE":
+			set_color("purple")
+		"SPECIAL":
+			set_color("rainbow")
+		"ACTION":
+			set_color("yellow")
+
+
 func set_color(new_color: String):
 	if not new_color in sprite.frames.get_animation_names():
 		printerr("Color ", new_color, " is not possible for chip")
 		return
+	color = new_color
 	sprite.animation = new_color
