240a9186dba305f2b4328f391ee038e9319eda19
2shady4u <jd16581556@gmail.com>
2020-08-23 21:32:56 +0200

    Add character info to HUD

--- src/UI/HUD.gd
+++ src/UI/HUD.gd
@@ -2,10 +2,18 @@ extends Control
 
 
 onready var turn_info = $TurnInfo
-onready var name_label = $MarginContainer/HBoxContainer/VBoxContainer/NameLabel
+onready var name_label = $MarginContainer/PanelContainer/HBoxContainer/BioVBox/NameLabel
 
 var current_character: Character = null
 
+onready var _perks_hbox := $MarginContainer/PanelContainer/HBoxContainer/BioVBox/PerksHBox
+
+onready var _attack_label := $MarginContainer/PanelContainer/HBoxContainer/StatsVBox/AttackHBox/AttackLabel
+onready var _healing_label := $MarginContainer/PanelContainer/HBoxContainer/StatsVBox/HealingHBox/HealingLabel
+
+var _perk_rect_resource := preload("res://src/UI/PerkRect.tscn")
+
+const DEFAULT_ICON_TEXTURE := "res://assets/graphics/perks/dash.png"
 
 func _ready():
 	LevelFlow.connect("current_character_changed", self, "_on_current_character_changed")
@@ -14,13 +22,36 @@ func _on_current_character_changed(new_character: Character):
 	print("Current character changed!")
 	current_character = new_character
 	turn_info.load_character(current_character)
-	
-	
-	if current_character.team == "PLAYER":
-		var character_name = Flow.get_orphan_value(current_character.type, "name", "None")
-		name_label.text = character_name
-	elif current_character.team == "ENEMY":
-		var character_name = Flow.get_enemy_value(current_character.type, "name", "None")
-		name_label.text = character_name
-	
 
+	for child in _perks_hbox.get_children():
+		_perks_hbox.remove_child(child)
+		child.queue_free()
+
+	var character_name := "MISSING NAME"
+	var perk_ids := []
+	var stats := {}
+	if current_character.team == "PLAYER":
+		var orphan_id : String = current_character.type
+		character_name = Flow.get_orphan_value(orphan_id, "name", "MISSING NAME")
+
+		perk_ids = Flow.get_orphan_value(orphan_id, "perk_ids", [])
+		stats = current_character.get_stats()
+
+	elif current_character.team == "ENEMY":
+		var enemy_id : String = current_character.type
+		character_name = Flow.get_enemy_value(enemy_id, "name", "MISSING NAME")
+
+		perk_ids = Flow.get_enemy_value(enemy_id, "perk_ids", [])
+		stats = current_character.get_stats()
+
+	name_label.text = character_name
+	for perk_id in perk_ids:
+		var perk_rect := _perk_rect_resource.instance()
+		_perks_hbox.add_child(perk_rect)
+
+		var icon_texture : String = Flow.get_perk_value(perk_id, "icon_texture", DEFAULT_ICON_TEXTURE)
+		perk_rect.texture = load(icon_texture)
+	
+	_attack_label.text = str(stats.get("damage", 0))
+	_healing_label.text = str(stats.get("healing", 0))
+	
---
+++ src/UI/PerkRect.gd
@@ -0,0 +1,2 @@
+extends TextureRect
+class_name class_perk_rect
